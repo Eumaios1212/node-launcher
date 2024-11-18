f4bde9507dfbd4acab8954bfc1d6e663d2a47658
Asmund <9491667-AsmundTHORSec@users.noreply.gitlab.com>
2024-11-18 21:04:43 +0000

    [ci] Add kubeconform linter

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -32,6 +32,8 @@ EOF
   fi
 }
 
+command -v kubeconform &>/dev/null || go install github.com/yannh/kubeconform/cmd/kubeconform@v0.6.7
+
 for NET in stagenet mainnet; do
   check_charts "$NET"
 done
@@ -42,7 +44,12 @@ done
 find . -type f -name 'Chart.yaml' -printf '%h\n' |
   while read -r CHART_DIR; do
     pushd "$CHART_DIR"
+    printf "Helm lint of %s\n" "$CHART_DIR"
     helm lint .
+    printf "Kubeconform of %s\n" "$CHART_DIR"
+    # If this triggers an issue for an unknown object type, check if a JSON schema
+    # already exists for it in https://github.com/datreeio/CRDs-catalog
+    helm template . | kubeconform -schema-location default -schema-location https://raw.githubusercontent.com/datreeio/CRDs-catalog/refs/heads/main/monitoring.coreos.com/servicemonitor_v1.json
     popd
   done
 
